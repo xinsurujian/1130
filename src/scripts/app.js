a850e0b3e8d98f8f30b115014694204aa93426b8
@xinsurujian <@1799711847@qq.com>
2016-11-30 10:17:59 +0800

    删除clear文件

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,7 +6,7 @@ require("./libs/swiper-3.3.1.min");
 require("./views/index");
 require("./views/guide");
 require("./views/home");
-require("./views/search");
+
 require("./views/my");
 
 SPA.config({
